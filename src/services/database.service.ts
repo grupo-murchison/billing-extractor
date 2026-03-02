adf07c8f5fde732858f1a769d1402c413f21c926
assertnano <ignaciokang@assertsolutions.com>
2026-03-02 18:33:17 -0300

    database.service log

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -32,25 +32,54 @@ export class DatabaseService {
     }
   }
 
+  // getProformaPendienteDeEnvio = async () => {
+  //   const url =
+  //     this.baseUrl + this.configService.get('BS_PROFORMA_PENDIENTE_DE_ENVIO');
+
+  //   let config = { ...this.axiosConfig };
+
+  //   return lastValueFrom(
+  //     this.httpService.get(url, config).pipe(
+  //       map((axiosResponse: AxiosResponse) => {
+  //         return axiosResponse.data;
+  //       }),
+
+  //       catchError((err) => {
+  //         this.logger.error('->getProformaPendienteDeEnvio', err);
+  //         return of(null);
+  //       }),
+  //     ),
+  //   );
+  // };
   getProformaPendienteDeEnvio = async () => {
-    const url =
-      this.baseUrl + this.configService.get('BS_PROFORMA_PENDIENTE_DE_ENVIO');
+  const path = this.configService.get<string>('BS_PROFORMA_PENDIENTE_DE_ENVIO');
+  const url = `${this.baseUrl}${path}`;
 
-    let config = { ...this.axiosConfig };
+  console.log('🔹 URL que se va a llamar:', url);
+  console.log('🔹 Config usada:', this.axiosConfig);
 
-    return lastValueFrom(
-      this.httpService.get(url, config).pipe(
-        map((axiosResponse: AxiosResponse) => {
-          return axiosResponse.data;
-        }),
+  const config = { ...this.axiosConfig };
 
-        catchError((err) => {
-          this.logger.error('->getProformaPendienteDeEnvio', err);
-          return of(null);
-        }),
-      ),
-    );
-  };
+  return lastValueFrom(
+    this.httpService.get(url, config).pipe(
+      map((axiosResponse: AxiosResponse) => {
+        console.log('Respuesta HTTP status:', axiosResponse.status);
+        console.log('Headers recibidos:', axiosResponse.headers);
+        console.log('Datos recibidos:', axiosResponse.data);
+        return axiosResponse.data;
+      }),
+      catchError((err) => {
+        console.error('Error en getProformaPendienteDeEnvio');
+        console.error('Message:', err.message);
+        console.error('Code:', err.code);
+        console.error('Response data:', err.response?.data);
+        console.error('Response status:', err.response?.status);
+        console.error('Config usada en la petición:', err.config);
+        return of(null);
+      }),
+    ),
+  );
+};
 
   async updateEstadoProforma(data: any) {
     const url = this.baseUrl + this.configService.get('BS_UPDATE_ESTADO_PROFORMA');
